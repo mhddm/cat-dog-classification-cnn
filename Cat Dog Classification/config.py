38823454a15c839f9f5fd488326d8a30c41acea6
mhddm <mhddm3@gmail.com>
2024-12-18 17:07:54 +0800

    Update config.py

--- Cat Dog Classification/config.py
+++ Cat Dog Classification/config.py
@@ -4,5 +4,8 @@ class Config:
     # Directory containing the image folders - cat, dog
     RAW_DATA_DIR = "C:\\Users\\mhddm\\Documents\\Datasets\\Pet Images"
     
+    # Directory to save the CNN model
+    SAVE_DIR = "C:\\Users\\mhddm\\Documents\\GitHub\\cat-dog-classification-cnn\\Cat Dog Classification\\Models"
+    
 
    
