1c6a0d1c57a9a8428c85a32dfaaa805c9d07f241
mhddm <mhddm3@gmail.com>
2024-12-19 20:44:19 +0800

    Update config.py

--- Cat Dog Classification/config.py
+++ Cat Dog Classification/config.py
@@ -7,5 +7,11 @@ class Config:
     # Directory to save the CNN model
     SAVE_DIR = "C:\\Users\\mhddm\\Documents\\GitHub\\cat-dog-classification-cnn\\Cat Dog Classification\\Models"
     
+    IMG_HEIGHT = 256
+    IMG_WIDTH = 256
+    BATCH_SIZE = 32
+    
+    EPOCHS = 10
+    
 
    
